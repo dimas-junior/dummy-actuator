3190ebd93a351b6b0a4c359515f3fd56c2fe39bc
dimas-junior <dimas.junior@maersk.com>
2026-03-14 15:12:09 -0300

    releas 0.0.2 - prometheus added

---
+++ src/test/java/com/example/dummyactuator/DummyActuatorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.dummyactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
